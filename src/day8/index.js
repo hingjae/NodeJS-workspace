7e000f513323fd0164adb19ce269423f32349f95
leejae honey <ljh23270@naver.com>
2022-07-06 16:59:25 +0900

    day8 done

--- src/day8/index.js
+++ src/day8/index.js
@@ -5,8 +5,8 @@ import movieRouter from "./movieRouter";
 
 const app = express();
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", process.cwd() + "/src/day8/view");
+app.use(express.urlencoded({ extended: true }));
 app.use("/", movieRouter);
-
 // Codesanbox does not need PORT :)
-app.listen(4001, () => console.log(`✅  Server Ready!`));
+app.listen(4002, () => console.log(`✅  Server Ready!`));
--- src/day8/movieController.js
+++ src/day8/movieController.js
@@ -6,7 +6,21 @@ import {
 } from "./db";
 
 export const home = (req, res) => {
-  return res.render("index");
+  const movies = getMovies();
+  // console.log(movies);
+  return res.render("index", { title: "Movies!", movies });
 };
-export const movieDetail = (req, res) => {};
-export const filterMovie = (req, res) => {};
+export const movieDetail = (req, res) => {
+  const { id } = req.params;
+  const movie = getMovieById(id);
+  return res.render("info", {
+    title: movie.title,
+    summary: movie.summary,
+    genres: movie.genres,
+  });
+};
+export const filterMovie = (req, res) => {
+  const movies = getMovies();
+  const { year, rating } = req.query;
+  return res.render("filter", { title: "Searching", movies, year, rating });
+};
