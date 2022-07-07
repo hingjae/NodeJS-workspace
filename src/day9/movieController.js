53d039e47051b68860487caaec84df69039e4aa9
leejae honey <ljh23270@naver.com>
2022-07-08 03:00:57 +0900

    done day9

--- src/day9/movieController.js
+++ src/day9/movieController.js
@@ -14,6 +14,20 @@ export const movieDetail = (req, res) => {
   return res.render("detail", { movie });
 };
 
+export const getUpload = (req, res) => {
+  return res.render("upload");
+};
+export const postUpload = (req, res) => {
+  const { title, synopsis, genres } = req.body;
+  const newMovie = {
+    title,
+    synopsis,
+    genres: genres.split(","),
+  };
+  addMovie(newMovie);
+  return res.redirect("/");
+};
+
 /*
 Write the controller or controllers you need to render the form
 and to handle the submission
--- src/day9/movieRouter.js
+++ src/day9/movieRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { home, movieDetail } from "./movieController";
+import { addMovie } from "./db";
+import { home, movieDetail, getUpload, postUpload } from "./movieController";
 
 const movieRouter = express.Router();
 
 // create the '/' route
 // create the /:id route
 // create the /add route (GET + POST)
+
+movieRouter.get("/", home);
+movieRouter.route("/:id(\\d+)").get(movieDetail);
+movieRouter.route("/add").get(getUpload).post(postUpload);
 
 export default movieRouter;
